e0fdee9895ae1a6795c43c9bb8fc4a8a6ee70c4c
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-10 12:27:52 +0530

    Added Parser Spec, should Return valid Item Entity when it is non exempted

--- test/com/thoughtworks/baseline/ParserTest.java
+++ test/com/thoughtworks/baseline/ParserTest.java
@@ -43,4 +43,22 @@ public class ParserTest {
 
         assertEquals(new Item("book", 12.49, true, false), parser.parse(tokens));
     }
+
+    @Test
+    public void shouldReturnNewItemInitializedWithGivenTokensWhenParserIsCalledForNonExemptedItem() {
+        String input = "1 book at 12.49";
+        ArrayList<String> tokens = new ArrayList<String>();
+        tokens.add("1");
+        tokens.add("music CD at");
+        tokens.add("12.49");
+        ArrayList<String> exemptedItems = new ArrayList<String>();
+        exemptedItems.add("book");
+        exemptedItems.add("chocolate bar");
+        exemptedItems.add("box of chocolates");
+        exemptedItems.add("chocolates");
+        exemptedItems.add("headache pills");
+        Parser parser = new Parser(input, exemptedItems);
+
+        assertEquals(new Item("music CD", 12.49, false, false), parser.parse(tokens));
+    }
 }
